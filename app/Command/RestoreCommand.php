00e01ded3a8422ded9a79bbc5161d7f9cfb02947
JPechberty <j.pechberty@gmail.com>
2025-02-10 23:48:39 +0100

    Add RestoreCommand

--- app/Command/RestoreCommand.php
+++ app/Command/RestoreCommand.php
@@ -2,8 +2,8 @@
 
 namespace Command;
 
+use Services\RestoreDbService;
 use Studoo\EduFramework\Commands\Extends\CommandBanner;
-use Studoo\EduFramework\Core\ConfigCore;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,14 +11,20 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[\Symfony\Component\Console\Attribute\AsCommand(name: 'restore', description: 'Renseigner la description de la commande restore')]
 class RestoreCommand extends \Studoo\EduFramework\Commands\Extends\CommandManage
 {
-	public function execute(InputInterface $input, OutputInterface $output): int
-	{
-		self::$stdOutput->writeln([
-		        CommandBanner::getBanner(),
-		        'Bienvenue dans la console ' . ConfigCore::getConfig('name'),
-		        '',
-		    ]);
-		    // Ajouter votre code ici
-		    return Command::SUCCESS;
-	}
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        self::$stdOutput->writeln([
+            CommandBanner::getBanner(),
+            'Lancement de la restauration de la base de données',
+            '',
+        ]);
+        // Ajouter votre code ici
+
+        RestoreDbService::restore();
+        self::$stdOutput->success('Restauration de la base de données terminée');
+
+        return Command::SUCCESS;
+    }
+
+
 }
